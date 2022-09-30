f4c4911254d3a4973d7c24195242d6a45a3bbce2
JulianP14 <julian.parera@hotmail.com>
2022-09-30 16:08:41 -0300

    entrega 02, correccion

--- 02/main.js
+++ 02/main.js
@@ -6,7 +6,12 @@ const ruta = path.resolve(__dirname, "./fyh.txt");
 const fecha = Date()
 
 fs.writeFile( ruta, fecha, (err, data) => {
-    if(err) console.log("ERROR", err.message);
+    if(err) console.log("ERROR ESCRITURA", err.message);
     console.log("Fecha agregada correctamente");
-    console.log(`La fecha es: ${fecha}`);
-})
+    // console.log(`La fecha es: ${fecha}`);
+
+    fs.readFile( ruta, "utf-8", (err, data) => {
+        if( err ) console.log("ERROR LECTURA", err.message);
+        console.log(data);
+    })
+});
