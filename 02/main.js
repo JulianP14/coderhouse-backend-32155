af02378b2319726654bef24e1c14e8586a8bab85
JulianP14 <julian.parera@hotmail.com>
2022-09-30 17:37:44 -0300

    entregable 02, comenzado

--- 02/main.js
+++ 02/main.js
@@ -1,17 +1,74 @@
-const fs = require ("fs");
-const path = require ("path");
+// ACTIVIDAD EN CLASE
+    const fs = require ("fs");
+    const path = require ("path");
+/*
+    const ruta = path.resolve(__dirname, "./fyh.txt");
+    const fecha = Date()
 
-const ruta = path.resolve(__dirname, "./fyh.txt");
+    fs.writeFile( ruta, fecha, (err, data) => {
+        if(err) console.log("ERROR ESCRITURA", err.message);
+        console.log("Fecha agregada correctamente");
+        // console.log(`La fecha es: ${fecha}`);
 
-const fecha = Date()
+        fs.readFile( ruta, "utf-8", (err, data) => {
+            if( err ) console.log("ERROR LECTURA", err.message);
+            console.log(data);
+        })
+    }); */
 
-fs.writeFile( ruta, fecha, (err, data) => {
-    if(err) console.log("ERROR ESCRITURA", err.message);
-    console.log("Fecha agregada correctamente");
-    // console.log(`La fecha es: ${fecha}`);
+// ENTREGABLE 02
 
-    fs.readFile( ruta, "utf-8", (err, data) => {
-        if( err ) console.log("ERROR LECTURA", err.message);
-        console.log(data);
+const ruta2 = path.resolve( __dirname, "./main.txt");
+
+const products = [
+    prod1 = {
+        title: "Banana",
+        price: 150,
+        id: 1
+    },
+    prod2 = {
+        title: "Manzana",
+        price: 175,
+        id: 2
+    },
+    prod3 = {
+        title: "Naranja",
+        price: 120,
+        id: 3
+    }
+]
+
+class Contenedor {
+    constructor (save, getById, getAll, deleteById, deleteAll) {
+        this.save = save,
+        this.getById = getById,
+        this.getAll = getAll,
+        this.deleteById = deleteById,
+        this.deleteAll = deleteAll
+    }
+
+    getSave (products) {
+        fs.writeFile( ruta2, JSON.stringify(products), (err, data) => {
+            if (err) console.log("ERROR ESCRITURA", err.message);
+            console.log("OK")
+            fs.readFile( ruta2, 'utf-8', (err, data) => {
+                if( err ) console.log("ERROR LECTURA", err.message);
+                console.log(data.prod1)
+            })
+        })
+    }
+}
+
+
+    fs.writeFile( ruta2, JSON.stringify(products), (err, data) => {
+        if (err) console.log("ERROR ESCRITURA", err.message);
+        console.log("OK")
+        fs.readFile( ruta2, 'utf-8', (err, data) => {
+            if( err ) console.log("ERROR LECTURA", err.message);
+            let dataString = JSON.parse(data);
+            console.log(dataString.title)
+               /*  data.forEach((prod) => {
+                    return console.log(prod)
+                }) */
+        })
     })
-});
